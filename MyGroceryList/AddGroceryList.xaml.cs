797e56bae1133324a7eeb67cb31881ffdb0491e2
Goran Mancic <gmancic@gmail.com>
2011-10-19 22:30:57 -0400

    Made few changes updated colors on main window

---
+++ MyGroceryList/AddGroceryList.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyGroceryList
+{
+    /// <summary>
+    /// Interaction logic for AddGroceryList.xaml
+    /// </summary>
+    public partial class AddGroceryList : Window
+    {
+        public AddGroceryList()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
